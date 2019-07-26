2c86e48a0e45a99d4b512f5869d0122933f61324
allforprogram <31071431+allforprogram@users.noreply.github.com>
2019-07-27 02:45:18 +0430

    first commit

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=remotemysql.com;dbname=UDZLFBDChh',
+    'username' => 'UDZLFBDChh',
+    'password' => 'q5JJKDWC28',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
